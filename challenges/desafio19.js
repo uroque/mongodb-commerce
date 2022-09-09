00f9ebcf2a9088bd066b443265f1c1c156b9ccee
Ulisses Roque <ulisses.roque@gmail.com>
2022-09-09 13:38:51 -0300

    adds requirement #19

---
+++ challenges/desafio19.js
@@ -0,0 +1,8 @@
+// Remova o item cebola de todos os sanduíches
+
+db.produtos.updateMany(
+  {},
+  { $pull: { ingredientes: "cebola" } },
+);
+
+db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
