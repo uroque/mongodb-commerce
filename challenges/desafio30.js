f1b8142fd0972573555fa19f1bbd5456628e59ed
Ulisses Roque <ulisses.roque@gmail.com>
2022-09-09 13:40:07 -0300

    adds requirement #30

---
+++ challenges/desafio30.js
@@ -0,0 +1,13 @@
+// Remova o campo curtidas do item Big Mac
+
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  {
+    $unset: { curtidas: "" },
+  },
+);
+
+db.produtos.find(
+  {},
+  { nome: 1, curtidas: 1, _id: 0 },
+);
